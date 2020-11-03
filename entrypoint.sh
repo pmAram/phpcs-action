c0ffb087a7c5df6538b78cef91f5e82bd532a8b3
pmAram <54321546+pmAram@users.noreply.github.com>
2020-11-03 10:32:40 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+if [ -z $STANDARD ]; then
+  STANDARD=Symfony
+fi
+
+if [ -z $MEM_LIMIT ]; then
+  MEM_LIMIT=512M
+fi
+
 cp /action/problem-matcher.json /github/workflow/problem-matcher.json
 
 echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/problem-matcher.json"
@@ -11,11 +19,11 @@ fi
 if [ -z "${INPUT_ENABLE_WARNINGS}" ] || [ "${INPUT_ENABLE_WARNINGS}" = "false" ]; then
     echo "Check for warnings disabled"
 
-    ${INPUT_PHPCS_BIN_PATH} -n --report=checkstyle
+    ${INPUT_PHPCS_BIN_PATH} -d memory_limit=$MEM_LIMIT --standard=$STANDARD -n --report=checkstyle
 else
     echo "Check for warnings enabled"
 
-    ${INPUT_PHPCS_BIN_PATH} --report=checkstyle
+    ${INPUT_PHPCS_BIN_PATH} -d memory_limit=$MEM_LIMIT --standard=$STANDARD --report=checkstyle
 fi
 
 status=$?
